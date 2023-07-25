ce3f87c71e2c827a2cf39263bd1fb4f5d60cb272
R4CE1962 <121911052+R4CE1962@users.noreply.github.com>
2023-07-25 19:45:17 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBDmpIWJ57U5k8NSfmxSi_iFMGLqpbI-sk",
+    authDomain: "lets-chat-web-app-21757.firebaseapp.com",
+    projectId: "lets-chat-web-app-21757",
+    storageBucket: "lets-chat-web-app-21757.appspot.com",
+    messagingSenderId: "972517762149",
+    appId: "1:972517762149:web:6cda3bd288ae30bb78d28c",
+    measurementId: "G-967793ZK9E"
+  };
+
+  firebaseConfig.initializeApp(firebaseConfig);
+
+  function add()
